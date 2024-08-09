0891f1408b7044b99ae047ace0c86694edced064
vshian <vvardhanreddy4@gmail.com>
2024-08-09 03:18:27 +0100

    moved MantineProvider to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Title } from '@mantine/core';
+import { Box, MantineProvider, Title } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { Calendar } from '@mantine/dates';
 import '@mantine/dates/styles.css';
@@ -16,41 +16,43 @@ type Event = {
 }
 
 type AppProps = {
-  events: Array<Event>;
-  onEventClick: (event: Event) => void;
+  events?: Array<Event>;
+  onEventClick?: (event: Event) => void;
 };
 
 function App(props: AppProps) {
   const [currentDate, setCurrentDate] = useState(new Date());
 
   return (
-    <Box>
-      <Header
-        title={dayjs(currentDate).format('MMMM YYYY')}
-        onPreviousClick={() => setCurrentDate(dayjs(currentDate).subtract(1, 'month').toDate())}
-        onNextClick={() => setCurrentDate(dayjs(currentDate).add(1, 'month').toDate())}
-        onTodayClick={() => setCurrentDate(new Date())}
-      />
-      <Calendar
-        static
-        maxLevel='month'
-        weekdayFormat='ddd'
-        date={currentDate}
-        renderDay={(date) => {
-          const day = date.getDate();
-          const event = props.events.find((event) => dayjs(event.date).isSame(date, 'day'));
-          return (
-            <>
-              <Title order={5} className='day-number'>{day}</Title>
-              {event &&
-                <DayEvent onClick={() => props.onEventClick?.(event)} title={event?.title} />
-              }
-            </>
-          );
-        }}
-      />
-    </Box>
+    <MantineProvider>
+      <Box>
+        <Header
+          title={dayjs(currentDate).format('MMMM YYYY')}
+          onPreviousClick={() => setCurrentDate(dayjs(currentDate).subtract(1, 'month').toDate())}
+          onNextClick={() => setCurrentDate(dayjs(currentDate).add(1, 'month').toDate())}
+          onTodayClick={() => setCurrentDate(new Date())}
+        />
+        <Calendar
+          static
+          maxLevel='month'
+          weekdayFormat='ddd'
+          date={currentDate}
+          renderDay={(date) => {
+            const day = date.getDate();
+            const event = props.events?.find((event) => dayjs(event.date).isSame(date, 'day'));
+            return (
+              <>
+                <Title order={5} className='day-number'>{day}</Title>
+                {event &&
+                  <DayEvent onClick={() => props.onEventClick?.(event)} title={event?.title} />
+                }
+              </>
+            );
+          }}
+        />
+      </Box>
+    </MantineProvider>
   );
 }
 
-export default App
+export default App;
